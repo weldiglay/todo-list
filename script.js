95091915f5807ee376e12497027d384446c2efe9
weldiglay <weldiglaymachadosouza@gmail.com>
2022-02-24 11:10:30 -0300

    desafios feito 1,2,3,4,5

--- script.js
+++ script.js
@@ -1,46 +1,10 @@
-const elementoLista = document.querySelector('ol');     // criado para pegar a ol.
-const elementoInput = document.querySelector('input');  // criado para pegar o input.
-const elementoBotao = document.querySelector('button'); // criado para pegar o botao.
+let botaoAdicionar = document.getElementById('criar-tarefa');
+let input = document.getElementById('texto-tarefa');
+let ol = document.getElementById('lista-tarefas');
 
-const tarefas = []; // criado uma array vazia. 
-
-
-function mostraTarefas() {  // cria umafunção 
-
-    elementoLista.innerHTML = ''; // limpa minha html , para inserir outro 
-
-    for (tarefa of tarefas) {   // criei um for para percorrer a array.
-        const elementoTarefa = document.createElement('li'); // criado para pegar o elemento li.
-        const textoTarefa = document.createTextNode(tarefa); // criado para pegar o texto que a pessoa digitar 
-
-        const elementoLink = document.createElement('a');
-
-        elementoTarefa.appendChild(textoTarefa);
-        elementoLista.appendChild(elementoTarefa);
-    }
-}
-
-mostraTarefas() // chamo a função
-
-function addTarefa() { // criado uma função
-    const textoTarefa = elementoInput.value; // pega o valor do input
-    tarefas.push(textoTarefa); // adiciona ao array 
-    elementoInput.value = ''; // limpa 
-
-    mostraTarefas() // chamo a função
-    }
-
-elementoBotao.setAttribute('onclick', 'addTarefa()');
-
-function deletaTarefa() {
-    const corGray = document.querySelector('gray');
-    if (corGray != null) {
-        corGray.classList.remove('gray');
-    }
-event.target.classList.add('gray');
-}
-
-
-
-   
-   
+botaoAdicionar.addEventListener('click', function() {
+let li = document.createElement('li');
+li.innerText = input.value;
+input.value = '';
+ol.appendChild(li);
+})
