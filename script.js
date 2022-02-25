2ce3137b08b61882ab8888db92a9931aa6b9ef34
weldiglay <weldiglaymachadosouza@gmail.com>
2022-02-25 20:57:25 -0300

    criado o click adicionar

---
+++ script.js
@@ -0,0 +1,10 @@
+let botaoAdicionar = document.getElementById('criar-tarefa');
+let input = document.getElementById('texto-tarefa');
+let ol = document.getElementById('lista-tarefas');
+
+botaoAdicionar.addEventListener('click', function() {
+    let li = document.createElement('li');
+    li.innerText = input.value;
+    ol.appendChild(li);
+});
+
