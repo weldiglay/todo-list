883348c4b5dde13f33d4a6e92c671baddf76a6f8
weldiglay <weldiglaymachadosouza@gmail.com>
2022-02-26 16:59:05 -0300

    desafio 6

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let botaoAdicionar = document.getElementById('criar-tarefa');
 let input = document.getElementById('texto-tarefa');
-let ol = document.getElementById('lista-tarefas');
+let ol = document.getElementById('lista-tarefas'); // ol  e a lista 
+
+
 
 botaoAdicionar.addEventListener('click', function() {
     let li = document.createElement('li');
     li.innerText = input.value;
     ol.appendChild(li);
-});
+    document.getElementById('texto-tarefa').value = null;
+});    
 
