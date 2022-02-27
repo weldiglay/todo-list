d7eac825839e1fe524b0cdaaffac03c35b2101e9
weldiglay <weldiglaymachadosouza@gmail.com>
2022-02-27 19:04:38 -0300

    desafio 9

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let botaoAdicionar = document.getElementById('criar-tarefa');
 let input = document.getElementById('texto-tarefa');
 let ol = document.getElementById('lista-tarefas'); // ol  e a lista pai d todos
+let apagaTudo = document.getElementById('apaga-tudo');
 
 botaoAdicionar.addEventListener('click', function() {
   let li = document.createElement('li');
@@ -9,7 +10,7 @@ botaoAdicionar.addEventListener('click', function() {
   document.getElementById('texto-tarefa').value = null;
 });
 
-// function pintarCinza () {
+// function pintarCinza (event) {
 //   const itemEscolhido = event.target;
 //   if (itemEscolhido.classlist.contains('selected')) {
 //     itemEscolhido.classlist.remove('selected');
@@ -20,14 +21,23 @@ botaoAdicionar.addEventListener('click', function() {
 // }
 // ol.addEventListener('click', pintarCinza);
 
- function pintarCinza(event) {
-     const naoEscolhido = document.querySelector('.selected');
-     if (naoEscolhido !== null) {
-        naoEscolhido.classList.toggle('selected');
-       }
-       const itemEscolhido = event.target;
-       itemEscolhido.classList.toggle('selected');
-     }
+function pintarCinza(event) {
+    const naoEscolhido = document.querySelector('.selected');
+    if (naoEscolhido !== null) {
+      naoEscolhido.classList.toggle('selected');
+      }
+      const itemEscolhido = event.target;
+      itemEscolhido.classList.toggle('selected');
+    }
 ol.addEventListener('click', pintarCinza);
 
+function riscaItem(event) {
+  const itemCompleto = event.target;
+  itemCompleto.classList.toggle('completed');
+}
+ol.addEventListener('dblclick', riscaItem);
 
+function apagaLista() {
+  ol.innerHTML = '';
+}
+apagaTudo.addEventListener('click', apagaLista);
