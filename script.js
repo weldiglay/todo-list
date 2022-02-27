b065e2b0bea0fea474c8c1c0bab379b9454dfaf6
weldiglay <weldiglaymachadosouza@gmail.com>
2022-02-27 11:10:30 -0300

    selected

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let botaoAdicionar = document.getElementById('criar-tarefa');
 let input = document.getElementById('texto-tarefa');
-let ol = document.getElementById('lista-tarefas'); // ol  e a lista pai de todos 
-
+let ol = document.getElementById('lista-tarefas'); // ol  e a lista pai d todos
 
 botaoAdicionar.addEventListener('click', function() {
     let li = document.createElement('li');
@@ -9,6 +8,37 @@ botaoAdicionar.addEventListener('click', function() {
     ol.appendChild(li);
     document.getElementById('texto-tarefa').value = null;
 });   
+
+function trocaColor(event) {
+    const trocaClick = event.target;
+    if (trocaClick.tagName === 'LI') {
+        clearLiColor();
+        trocaClick.style.backgroundColor = 'gray';
+    }
+}
+document.addEventListener('click', trocaColor);
+
+function liCinza(event) {
+    const selectedLi = document.querySelectorAll('.selected');
+        if (selectedLi !== null) {
+            selectedLi.remove('selected');
+        }
+        event.target.classList.add('selected'); 
+}
+document.addEventListener('click', liCinza);
+
+
+function LiRiscado(event) {
+    event.target.classList.toggle('completed');
+}
+document.addEventListener('dblclick', LiRiscado);
+
+
+
+
+
+
+
 
 
 
