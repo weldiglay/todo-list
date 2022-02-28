05ac9946f5ce134549a03a611595e6eeebc2cdca
weldiglay <weldiglaymachadosouza@gmail.com>
2022-02-28 18:42:39 -0300

    desafio 11,variaveis criadas, condiçao e o for

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let botaoAdicionar = document.getElementById('criar-tarefa');
 let input = document.getElementById('texto-tarefa');
-let ol = document.getElementById('lista-tarefas'); // ol  e a lista pai d todos
+let ol = document.getElementById('lista-tarefas');   // A ol e aa Lista PAI de Todos
 let apagaTudo = document.getElementById('apaga-tudo');
+let salvaTudo = document.getElementById('salvar-tarefas');
+let ClearComplete = document.getElementById('remover-finalizados'); 
+
 
 botaoAdicionar.addEventListener('click', function() {
   let li = document.createElement('li');
@@ -9,6 +12,48 @@ botaoAdicionar.addEventListener('click', function() {
   ol.appendChild(li);
   document.getElementById('texto-tarefa').value = null;
 });
+
+function pintarCinza(event) {
+  const naoEscolhido = document.querySelector('.selected');
+  if (naoEscolhido !== null) {
+    naoEscolhido.classList.toggle('selected');
+    }
+  const itemEscolhido = event.target;
+  itemEscolhido.classList.toggle('selected');
+  }
+ol.addEventListener('click', pintarCinza);
+
+
+function riscaItem(event) {
+  const itemCompleto = event.target;
+  itemCompleto.classList.toggle('completed');
+}
+ol.addEventListener('dblclick', riscaItem);
+
+
+function apagaLista() {
+  ol.innerHTML = '';
+}
+apagaTudo.addEventListener('click', apagaLista);
+
+const ClearCompleted = document.getElementById('remover-finalizados'); // variavel
+ClearCompleted.addEventListener('click', function () {                 // pegando a variavel para add o envento click
+    const listRemove = document.querySelectorAll('li');                // variavel para remover todos do filho li
+    for (let index = 0; index < listRemove.length; index += 1) {       // for lacos e loop, menor e menos 
+      const taskRemove = listRemove[index];                             // variavel taskremove e ela mesma com o arrayindex
+
+      if(taskRemove.className.includes('completed')){                   //condição se                
+      ol.removeChild(taskRemove);                                       // pega a lista pai e remove o filho resultado
+}
+  }
+});
+
+
+
+
+
+
+
 
 // function pintarCinza (event) {
 //   const itemEscolhido = event.target;
@@ -20,24 +65,3 @@ botaoAdicionar.addEventListener('click', function() {
 //   }
 // }
 // ol.addEventListener('click', pintarCinza);
-
-function pintarCinza(event) {
-    const naoEscolhido = document.querySelector('.selected');
-    if (naoEscolhido !== null) {
-      naoEscolhido.classList.toggle('selected');
-      }
-      const itemEscolhido = event.target;
-      itemEscolhido.classList.toggle('selected');
-    }
-ol.addEventListener('click', pintarCinza);
-
-function riscaItem(event) {
-  const itemCompleto = event.target;
-  itemCompleto.classList.toggle('completed');
-}
-ol.addEventListener('dblclick', riscaItem);
-
-function apagaLista() {
-  ol.innerHTML = '';
-}
-apagaTudo.addEventListener('click', apagaLista);
