31330c5e7dc653ca4f083e0d051616f472beb855
weldiglay <weldiglaymachadosouza@gmail.com>
2022-04-01 22:43:48 -0300

    update

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-let botaoAdicionar = document.getElementById('criar-tarefa');
-let input = document.getElementById('texto-tarefa');
-let ol = document.getElementById('lista-tarefas');   // A ol e aa Lista PAI de Todos
-let apagaTudo = document.getElementById('apaga-tudo');
-let btnSalva = document.getElementById('salvar-tarefas');
-let ClearComplete = document.getElementById('remover-finalizados'); 
-let ApagaSelecionado = document.getElementById('remover-selecionado');
-let botaoDown = document.getElementById('mover-baixo');
-let botaoUp   = document.getElementById('mover-cima');
+const botaoAdicionar = document.getElementById('criar-tarefa');
+const input = document.getElementById('texto-tarefa');
+const ol = document.getElementById('lista-tarefas'); // A ol e aa Lista PAI de Todos
+const apagaTudo = document.getElementById('apaga-tudo');
+const btnSalva = document.getElementById('salvar-tarefas');
+const ClearComplete = document.getElementById('remover-finalizados');
+const ApagaSelecionado = document.getElementById('remover-selecionado');
+const botaoDown = document.getElementById('mover-baixo');
+const botaoUp = document.getElementById('mover-cima');
 
-
-botaoAdicionar.addEventListener('click', function() {
-  let li = document.createElement('li');
+botaoAdicionar.addEventListener('click', () => {
+  const li = document.createElement('li');
   li.innerText = input.value;
   ol.appendChild(li);
   document.getElementById('texto-tarefa').value = null;
@@ -20,10 +19,10 @@ function pintarCinza(event) {
   const naoEscolhido = document.querySelector('.selected');
   if (naoEscolhido !== null) {
     naoEscolhido.classList.toggle('selected');
-    }
+  }
   const itemEscolhido = event.target;
   itemEscolhido.classList.toggle('selected');
-  }
+}
 ol.addEventListener('click', pintarCinza);
 
 function riscaItem(event) {
@@ -38,26 +37,26 @@ function apagaLista() {
 apagaTudo.addEventListener('click', apagaLista);
 
 const ClearCompleted = document.getElementById('remover-finalizados'); // variavel
-ClearCompleted.addEventListener('click', function () {                 // pegando a variavel para add o envento click
-    const listRemove = document.querySelectorAll('li');                // variavel para remover todos do filho li
-    for (let index = 0; index < listRemove.length; index += 1) {       // for lacos e loop, menor e menos 
-      const taskRemove = listRemove[index];                             // variavel taskremove e ela mesma com o arrayindex
+ClearCompleted.addEventListener('click', () => { // pegando a variavel para add o envento click
+  const listRemove = document.querySelectorAll('li'); // variavel para remover todos do filho li
+  for (let index = 0; index < listRemove.length; index += 1) { // for lacos e loop, menor e menos
+    const taskRemove = listRemove[index]; // variavel taskremove e ela mesma com o arrayindex
 
-      if(taskRemove.className.includes('completed')){                   //condição se                
-      ol.removeChild(taskRemove);                                       // pega a lista pai e remove o filho resultado }                                                                     //  ajuda hector , lucas 
-}
+    if (taskRemove.className.includes('completed')) { // condição se
+      ol.removeChild(taskRemove); // pega a lista pai e remove o filho resultado }                                                                     //  ajuda hector , lucas
     }
+  }
 });
 
 const ApagaCompleted = document.getElementById('remover-selecionado'); // variavel
-ApagaCompleted.addEventListener('click', function () {                 // pegando a variavel para add o envento click
-    const listRemove = document.querySelectorAll('li');                // variavel para remover todos do filho li
-    for (let index = 0; index < listRemove.length; index += 1) {       // for lacos e loop, menor e menos 
-      const taskRemove = listRemove[index];                             // variavel taskremove e ela mesma com o arrayindex
-      if(taskRemove.className.includes('selected')){                   //condição se                
-      ol.removeChild(taskRemove);                                       // pega a lista pai e remove o filho resultado }                                                                     //  ajuda hector , lucas 
-}
+ApagaCompleted.addEventListener('click', () => { // pegando a variavel para add o envento click
+  const listRemove = document.querySelectorAll('li'); // variavel para remover todos do filho li
+  for (let index = 0; index < listRemove.length; index += 1) { // for lacos e loop, menor e menos
+    const taskRemove = listRemove[index]; // variavel taskremove e ela mesma com o arrayindex
+    if (taskRemove.className.includes('selected')) { // condição se
+      ol.removeChild(taskRemove); // pega a lista pai e remove o filho resultado }                                                                     //  ajuda hector , lucas
     }
+  }
 });
 
 btnSalva.onclick = () => {
@@ -66,7 +65,6 @@ btnSalva.onclick = () => {
     item.push(ol.children[index].outerHTML);
   }
   localStorage.setItem('ol', JSON.stringify(item));
-  
 };
 
 botaoUp.onclick = () => {
@@ -113,10 +111,9 @@ botaoDown.onclick = () => {
 
 window.onload = () => {
   if (localStorage.ol) {
-    let saveTry = JSON.parse(localStorage.getItem('ol'));
+    const saveTry = JSON.parse(localStorage.getItem('ol'));
     for (let i = 0; i < saveTry.length; i += 1) {
       ol.insertAdjacentHTML('beforeend', saveTry[i]);
-  } 
-}
-}
-
+    }
+  }
+};
